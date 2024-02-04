46f0f8cb2f262e93275b2967b2e3ac10d8edd0d9
Amish <amish.tufail2002@gmail.com>
2024-02-04 21:51:17 +0500

    Initial Commit

---
+++ ViewsMastery/ViewsMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsMasteryApp.swift
+//  ViewsMastery
+//
+//  Created by Amish Tufail on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
